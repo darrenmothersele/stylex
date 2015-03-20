7dd1c4770f60760de172c5d28329c78c2b6128fd
Darren <darren@darrenmothersele.com>
2015-03-20 10:46:55 +0000

    first version

--- src/Stylex/Application.php
+++ src/Stylex/Application.php
@@ -9,8 +9,50 @@ class Application extends \Silex\Application {
   public function __construct(array $values = []) {
     parent::__construct($values);
 
+    // $this['debug'] = TRUE;
+
+    // Default values
+    $values += [
+      'templates' => 'templates',
+      'data' => 'data',
+      'content' => 'content',
+      'twig.options' => ['strict_variables' => FALSE],
+    ];
+
+    $this->register(new \Silex\Provider\TwigServiceProvider(), array(
+      'twig.path' => $values['templates'],
+      'twig.options' => $values['twig.options'],
+    ));    
+    $yaml = new \Symfony\Component\Yaml\Parser();
+
+    // Load all data files
+    if (file_exists($values['data'])) {
+      $finder = new \Symfony\Component\Finder\Finder();
+      $finder->files()->in($values['data'])->name('*.yml');
+      foreach ($finder as $file) {
+        $data = $yaml->parse($file->getContents());
+        $this['twig']->addGlobal($file->getBasename('.yml'), $data);
+      }
+    }
+
+    // Load all sample content
+    $content = [];
+    if (file_exists($values['content'])) {
+      $finder = new \Symfony\Component\Finder\Finder();
+      $finder->files()->in($values['content'] . '/*')->name('*.md');
+      foreach ($finder as $file) {
+        list(, $data, $body) = explode("---\n", $file->getContents());
+        $data = $yaml->parse($data);
+        $data['content'] = \Michelf\Markdown::defaultTransform($body);
+        $content[basename(dirname($file->getPathname()))][$file->getBasename('.md')] = $data;
+      }
+    }
+    $this['twig']->addGlobal('content', $content);
+
+    // Setup the front controller
     $this->get('/{page}', function ($page) {
-      return 'Hello ' . $this->escape($page);
+      $this['twig']->addGlobal('current_page', '/' . (($page == 'index') ? '' : $page));
+      return $this->render($page . '.html');
     })->value('page', 'index');
   }
 }
